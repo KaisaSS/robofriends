45a8ded8d6a74052a57fde5733e67b441b341acb
Kaisa <parvej@Kaisas-MacBook-Pro.local>
2021-09-20 12:16:06 +0100

    Add redux async actions

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,53 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CardList from "../Components/CardList";
 import SearchBox from "../Components/SearchBox";
 import Scroll from "../Components/Scroll";
 import ErrorBoundry from "../Components/ErrorBoundry";
 import "./App.css";
-import { setSearchField } from "../actions";
+import { setSearchField, requestRobots } from "../actions";
 
 const App = () => {
-  const [robots, setRobots] = useState([]);
   const dispatch = useDispatch();
-  const { searchField } = useSelector((state) => state);
+  const { searchField } = useSelector((state) => state.searchRobots);
+  const { robots, isPending } = useSelector((state) => state.requestRobots);
 
   const onSearchChange = (event) => {
     dispatch(setSearchField(event.target.value));
   };
 
+  const onRequestRobots = () => {
+    dispatch(requestRobots());
+  };
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        setRobots(users);
-      });
+    onRequestRobots();
   }, []);
 
   const filreredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase());
   });
 
-  if (!robots.length) {
-    return <h1>Loading...</h1>;
-  } else {
-    return (
-      <div className="tc">
-        <div className="title-container">
-          <h1 className="title logo">
-            <i className="fas fa-robot"></i>
-          </h1>
-          <h1 className="title">RoboFriends</h1>
-        </div>
-        <SearchBox searchChange={onSearchChange} />
-        <Scroll>
-          <ErrorBoundry>
-            <CardList robots={filreredRobots} />
-          </ErrorBoundry>
-        </Scroll>
+  return isPending ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="tc">
+      <div className="title-container">
+        <h1 className="title logo">
+          <i className="fas fa-robot"></i>
+        </h1>
+        <h1 className="title">RoboFriends</h1>
       </div>
-    );
-  }
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filreredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
 };
-
 export default App;
